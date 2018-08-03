dc15d2c3e8ff5b85fea24e539df757cc50488f49
Antonio Garza <develanton@gmail.com>
2018-08-03 11:40:53 -0500

    fixed conflict in register

--- public/js/register.js
+++ public/js/register.js
@@ -4,20 +4,11 @@ var iduser;
 $("#registerButton").on("click", function () {
   console.log("DUDE");
   var userReg = {
-<<<<<<< HEAD
-    name: $("#firstName").val(),
-    last: $("#lastName").val(),
-    email: $("#email").val(),
-    password: $("#userPass").val(),
-    birthday: $("#birthday").val(),
-    gender: $("#gender").val()
-=======
     nombre: $("#nombreUsuarioR").val(),
     apellido: $("#apellidoUsuario").val(),
     email: $("#emailU").val(),
     password: $("#userPassR").val(),
     nacimiento: $("#nacimiento").val()
->>>>>>> abes
   };
 
   $.ajax({
@@ -27,19 +18,6 @@ $("#registerButton").on("click", function () {
     success: function (req) {
       if (req.mensaje == "USUARIO") {
         console.log("USUARIO EQUIVOCADO");
-<<<<<<< HEAD
-        alert("Usuario ya Existe favor de Reigistrarse con nuevo usuario");
-        url = "/login";
-        $(location).attr("href", url);
-      } else {
-        alert("Usuario Registrado Correctamente Favor de hacer Login");
-        url = "/login";
-        $(location).attr("href", url);
-      }
-
-      console.log(msg.mensaje);
-
-=======
         alert("Usuario ya Existe favor de hacer login con su usuario");
         url = "/login";
         $(location).attr("href", url);
@@ -58,7 +36,6 @@ $("#registerButton").on("click", function () {
         $("#profile").addClass("show");
         $("#profile").addClass("active");
       }
->>>>>>> abes
     }
   });
 
